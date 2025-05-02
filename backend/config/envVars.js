24f94008e5c64e5eee7376cf48a1741c5f62372f
richard.ali <richardali@ymail.com>
2025-05-02 16:28:53 +0100

    Added mongo

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV_VARS = {
-	MONGO_URI: process.env.MONGO_URI,
+	MONGO_URI: process.env.MONGO_URI || 'mongodb://mongo:EZcznSmCZoGPJtZkFlmukyLpYsZeFSle@mongodb.railway.internal:27017',
 	PORT: process.env.PORT || 5000,
 	JWT_SECRET: process.env.JWT_SECRET,
 	NODE_ENV: process.env.NODE_ENV,
